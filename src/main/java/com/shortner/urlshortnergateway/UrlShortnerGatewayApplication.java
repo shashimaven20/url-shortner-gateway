f2e9c8e0e7c745b0d6bde171fab231cb235677c2
shashimaven20 <shashimaven2.0@gmail.com>
2026-03-17 00:26:29 +0530

    setup

---
+++ src/main/java/com/shortner/urlshortnergateway/UrlShortnerGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.shortner.urlshortnergateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShortnerGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrlShortnerGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shortner/urlshortnergateway/UrlShortnerGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shortner.urlshortnergateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortnerGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
