9e3dfe9604aef7ba3b1f5f43157045498f38ed0f
shashimaven20 <shashimaven2.0@gmail.com>
2026-03-18 01:14:57 +0530

    request filter added

---
+++ src/main/java/com/shortner/urlshortnergateway/Config/RateLimiterConfig.java
@@ -0,0 +1,21 @@
+package com.shortner.urlshortnergateway.Config;
+
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Mono;
+
+import java.util.Objects;
+
+@Configuration
+public class RateLimiterConfig {
+
+    @Bean
+    public KeyResolver userKeyResolver() {
+        return exchange -> Mono.just(
+                Objects.requireNonNull(exchange.getRequest()
+                                .getRemoteAddress())
+                        .getAddress()
+                        .getHostAddress());
+    }
+}
--- src/main/java/com/shortner/urlshortnergateway/UrlShortnerGatewayApplication.java
+++ src/main/java/com/shortner/urlshortnergateway/UrlShortnerGatewayApplication.java
@@ -8,6 +8,7 @@ public class UrlShortnerGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UrlShortnerGatewayApplication.class, args);
+        System.out.println("Hi");
     }
 
 }
