3ccf921d28b4f4e60e55a7cbc8a3d8ff8d172515
shashimaven20 <shashimaven2.0@gmail.com>
2026-03-29 20:33:58 +0530

    auth fix

--- src/main/java/com/shortner/urlshortnergateway/Filter/JwtAuthenticationFilter.java
+++ src/main/java/com/shortner/urlshortnergateway/Filter/JwtAuthenticationFilter.java
@@ -1,14 +1,25 @@
 package com.shortner.urlshortnergateway.Filter;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.security.Keys;
+
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
-import org.springframework.stereotype.Component;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
+
 import reactor.core.publisher.Mono;
+
+import java.security.Key;
 
 @Component
 public class JwtAuthenticationFilter extends AbstractGatewayFilterFactory<JwtAuthenticationFilter.Config> {
+
+    private final String SECRET = "mysecretkeymysecretkeymysecretkey123"; // must match auth service
+    private final Key key = Keys.hmacShaKeyFor(SECRET.getBytes());
 
     public JwtAuthenticationFilter() {
         super(Config.class);
@@ -19,6 +30,7 @@ public class JwtAuthenticationFilter extends AbstractGatewayFilterFactory<JwtAut
 
     @Override
     public GatewayFilter apply(Config config) {
+
         return (exchange, chain) -> {
 
             String authHeader = exchange.getRequest()
@@ -32,7 +44,26 @@ public class JwtAuthenticationFilter extends AbstractGatewayFilterFactory<JwtAut
 
             String token = authHeader.substring(7);
 
-            // TODO validate JWT token
+            try {
+
+                Claims claims = Jwts.parserBuilder()
+                        .setSigningKey(key)
+                        .build()
+                        .parseClaimsJws(token)
+                        .getBody();
+
+                // optional: get user email
+                String user = claims.getSubject();
+
+                System.out.println("Valid token for user: " + user);
+
+            } catch (JwtException e) {
+
+                System.out.println("Invalid JWT token");
+
+                exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                return exchange.getResponse().setComplete();
+            }
 
             return chain.filter(exchange);
         };
